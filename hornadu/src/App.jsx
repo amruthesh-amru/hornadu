39904bc93fce719d34e65ba4a9354dd86ea30d07
amruthesh <amrutheshamru16@gmail.com>
2024-04-16 21:23:07 +0530

    a

--- hornadu/src/App.jsx
+++ hornadu/src/App.jsx
@@ -3,7 +3,14 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./appRouter";
 import Footer from "./components/Footer";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-99K87R74QB");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+  title: "Home Page",
+});
 function App() {
   return (
     <>
